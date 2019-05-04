96cd586aeea78cedefe333815ae0022ab8a23bf3
AndreaLG123 <andreamontserratlunagarcia@hotmail.com>
2019-05-04 14:17:46 -0500

    Configuración para inicio

--- server/index.js
+++ server/index.js
@@ -12,5 +12,16 @@ app.use(bodyParser.json());
 //const GatosRoutes = require('./routes/gatosRoutes'); //Forma larga
 require('./routes/gatosRoutes')(app); //Forma corta
 
+
+//Usa las vistas de react
+if(process.env.NODE_ENV === 'production')
+{
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req, res) =>{
+		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+	});
+}
+
 app.listen(process.env.PORT || 5000); // Puerto: 5000 -> back, 3000 -> front 
 
