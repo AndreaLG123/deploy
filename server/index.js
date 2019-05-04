edafe47a534c27d911e184042ceed86182c31cbe
AndreaLG123 <andreamontserratlunagarcia@hotmail.com>
2019-05-04 14:29:09 -0500

    Configuración para inicio

--- server/index.js
+++ server/index.js
@@ -14,13 +14,12 @@ require('./routes/gatosRoutes')(app); //Forma corta
 
 
 //Usa las vistas de react
-if(process.env.NODE_ENV === 'production')
-{
-	app.use(express.static('client/build'));
-	const path = require('path');
-	app.get('*', (req, res) =>{
-		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-	});
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    })
 }
 
 app.listen(process.env.PORT || 5000); // Puerto: 5000 -> back, 3000 -> front 
